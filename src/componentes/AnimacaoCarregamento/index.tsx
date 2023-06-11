a8d9c0b2161e56bec2d11ead76d26ade41474fb6
Paulo Henrique Evangelista <phfernandesev@gmail.com>
2023-06-10 23:41:53 -0300

    Atualizar animacao carregamento

--- src/componentes/AnimacaoCarregamento/index.tsx
+++ src/componentes/AnimacaoCarregamento/index.tsx
@@ -3,7 +3,7 @@ import styles from './AnimacaoCarregamento.module.scss';
 
 const AnimacaoCarregamento: React.FC = () => {
   return (
-    <div className={styles['dot-pulse']} />
+    <div className={styles['animacao-pulso']} />
   )
 }
 
